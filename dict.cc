dfad979f978ba2c9cd7429f76342f807ef9ccc99
Rhygaer <96240509+Rhygaer@users.noreply.github.com>
2021-12-16 17:05:37 +0100

    Update dict.cc

--- dict.cc
+++ dict.cc
@@ -18,13 +18,12 @@
 	--grad2: #1b6fd5;
 	
 }
+::selection {
+  background-color: var(--grad1);
+}
 
-/* *, *::before, *::after {
-	margin: 0;
-	padding: 0;
-	box-sizing:border-box;
 
-} */
+
 body {
 	position: relative;
 	width: 100%;
@@ -32,7 +31,8 @@ body {
   	justify-content: center;
 	flex-direction:column;
 	
-	background-image: url("https://www.toptal.com/designers/subtlepatterns/patterns/dot-grid.png");
+	background-image: url("https://www.toptal.com/designers/subtlepatterns/patterns/dot-grid.png"); 
+/* 	background-image: url("https://www.toptal.com/designers/subtlepatterns/patterns/cartographer.png");  */
   	background-repeat: repeat;
 /* 	background: blue; */
 }
@@ -200,9 +200,9 @@ background-image: linear-gradient(45deg, #08e9ea 0%, #2AF598 100%);
 .noline{
 	background: var(--grey200)!important;
 	color: var(--grey800);
+	
 	border-bottom: 0 !important;
 }
-
 
 
 
@@ -210,7 +210,8 @@ background-image: linear-gradient(45deg, #08e9ea 0%, #2AF598 100%);
 /**************** ENTRIES */
 
 .td7cml{
-	background: var(--grey000);
+	background: var(--grey000)!important;;
+	border-bottom: 0px solid var(--grey100);
 	
 }
 
@@ -218,20 +219,17 @@ background-image: linear-gradient(45deg, #08e9ea 0%, #2AF598 100%);
 	border: 3px solid #bf2b2b;
 }
 .td7nl{
-	background: var(--grey100);
-/* 	background: red; */
+	background: var(--grey100)!important;
 	border-bottom:1px solid var(--grey000);
-	border: 0 solid red
+	border-left: 0px solid var(--grey300) !important;
+	height: 20px;
 
 }
 .td7cmr{
-/* 	content:"🔊" */
-	background: var(--grey000);
 
-}
-.td7cmr {
-	
-	
+	background: var(--grey000)!important;;
+	border-bottom: 2px solid var(--grey100);
+
 }
 
 
@@ -241,6 +239,164 @@ a kbd{
 	color: var(--primary300);
 }
 
+
+
+
+
+/*** Icons ***/
+
+
+img[src="https://www4.dict.cc/img/but_info.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/zGhgFV55/Info.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+	
+}
+
+img[src="https://www4.dict.cc/img/but_speech.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/CKVfPSL8/Speaker.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+	
+}
+
+
+/* Leo */
+img[src="https://www4.dict.cc/img/but_l.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/brzWf31T/Leo.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+/* PONS */
+img[src="https://www4.dict.cc/img/but_p.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/4xCqQ63B/Pons.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+
+
+/* Merriam */
+img[src="https://www4.dict.cc/img/but_m.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/Mpy4PnMx/Merriam.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+/* Duden */
+img[src="https://www4.dict.cc/img/but_d.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/PfRBPsnG/Duden.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+/* Free */
+img[src="https://www4.dict.cc/img/but_t.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/fTS1d2MV/Free.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+/* verbformen / Netzverb */
+img[src="https://www4.dict.cc/img/but_n.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/JhxF8S52/verbformen-Netzverb.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+/* Reverso */
+img[src="https://www4.dict.cc/img/but_r.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/3wYqP3mG/Reverso.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+
+/* Wiktionary */
+img[src="https://www4.dict.cc/img/but_w.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/gJ0T24Mk/Wiktionary.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+
+/* Cambridge */
+img[src="https://www4.dict.cc/img/but_c.gif"]{
+	padding: 9px;
+	background-color:red ;
+	width:0px !important;
+	height: 0px !important;
+	content: "🔊" !important;
+	background: url('https://i.postimg.cc/Zn2gg3xY/Cambridge.png')!important;
+	background-size:auto;
+	border: 0 solid #eee !important;;
+}
+
+
+
+
+
+/***** icon pop up menu *****/
+.overDiv > div[style="border:1px solid #333;background-color:#ccc"]{
+	border:0 solid #eee !important;
+}
+.cmenu {
+	background: var(--grey100);
+	border: 0 !important; 
+	border-radius 4px !important;
+	box-shadow: 0 8px 16px #58629b94;
+}
 
 
 /* Titles*/
@@ -270,5 +426,58 @@ a kbd{
 }
 .td1nl a{
 	
+}
+
+
+
+/***************** Examples *******************/
+#tabmi1 {
+	background: var(--primary500);
+	height: 28px;
+	display: inline-block;
 	
+	
+}
+#tabmi2{
+	color: var(--primary500);
+	
+}
+
+#maincontent div[style="width:729px;"] h3{
+/* 	background: red !important; */
+	height: 30px;
+	border: 0 !important;
+}
+
+#mi1{
+	
+}
+.hl3{
+	color:var(--primary300);
+	margin-bottom:8px;
+}
+.hl3 a b{
+	background:red;
+}
+
+#maincontent table[width="728"]{
+	
+}
+.td1{
+	background: var(--primary300);
+	
+}
+img[src="https://www4.dict.cc/img/hr4.gif"]{
+	display:none;
+}
+
+
+
+/***** Footer *******/
+.aftertable{
+	margin-top: 20px;
+	color: var(--grey500)
+}
+.aftertable a{
+	color: var(--grey500)
 }
